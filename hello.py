9ec35f84d94033f2651563dd5cbc7293ecd24a37
andryuha77 <andryuha1977@gmail.com>
2016-10-15 15:28:35 +0100

    .js file linked pars bug fixet

--- hello.py
+++ hello.py
@@ -9,9 +9,13 @@ def hello():
 def name():
     return "Hello " + fl.request.form["name"] + "!"	
 	
-
 if __name__ == "__main__":
     app.run()
-
-
-
+    
+#@app.route("/BMI")
+#def BMICalculate():
+#    return app.send_static_file('BMI.js')
+#
+#@app.route("/name", methods=["GET", "POST"])
+#def name():
+#    return "BMI : " + fl.request.form["BMI"] + "!"	

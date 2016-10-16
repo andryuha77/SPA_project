af7939a1d40b817db2a8958999e1cbe5b2a28978
andryuha77 <andryuha1977@gmail.com>
2016-10-16 23:23:17 +0100

    added style

--- hello.py
+++ hello.py
@@ -4,13 +4,16 @@ app = fl.Flask(__name__)
 @app.route("/")
 def hello():
     return app.send_static_file('index.html')
-	
+
+
 @app.route("/name", methods=["GET", "POST"])
 def name():
-    return "Hello " + fl.request.form["name"] + "!"	
+#    return "Hello " + fl.request.form["name"] + "!!!"
+    return app.send_static_file('index.html', **locals())
 	
-if __name__ == "__main__":
+if __name__== "__main__":
     app.run()
+
     
 #@app.route("/BMI")
 #def BMICalculate():

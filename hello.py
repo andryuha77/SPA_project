d498c65ce4c56e30dcb61175978023ee7990abdb
ANDREI PETRUK - Student <G00328108@gmit.ie>
2016-11-02 10:50:46 +0000

    remove slider

--- hello.py
+++ hello.py
@@ -9,13 +9,9 @@ app = fl.Flask(__name__)
 def hello():
     return app.send_static_file('index.html')
 
-@app.route("/perms", methods=["GET", "POST"])
-def perms():
-	perms = [''.join(p) for p in it.permutations(fl.request.values["userinput"])]
-	return ''.join("BMI")
-
-#    return "Hello " + fl.request.form["name"] + "!!!"
-#    return app.send_static_file('index.html', **locals())
+@app.route("/name", methods=["GET", "POST"])
+def name():
+    return "Hello " + fl.request.form["name"] + "!"
 
 if __name__ == "__main__":
     app.run()
